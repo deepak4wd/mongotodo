68848f3a017a7293f633041f5d2c3ffd02aff4a2
Deepak Kulkarni <deepak4wd@gmail.com>
2018-12-18 23:33:48 +0530

    added server

---
+++ server.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyparser = require("body-parser");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/TodoDeep");
+
+var ToDo = mongoose.model("todo1", {
+text: {type: String, required: true, minlength: 1, trim: true },
+completed: {type: Boolean, default: false},
+createdat: {type: Number, default: 18122018}
+});
+
+var app = express();
+app.use(bodyparser.json());
+
+app.post('/todos', (req, resp) => {
+    var newtodo = new ToDo({text: req.body.text});
+
+    newtodo.save().then((obs)=>{
+            console.log("inserted sucessfully");
+            resp.send(obs);
+        }).catch((err)=>{
+            console.log("inserted fail");
+            resp.status(400).send(err);
+
+        });
+});
+
+app.listen(3000, ()=>{
+    console.log("started on port 3000");
+});
